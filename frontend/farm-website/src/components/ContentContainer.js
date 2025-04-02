e23921fc96859b4f722883546e937d6381a20006
atpavithran <pavithranmurugavel@gmail.com>
2025-04-02 16:14:34 +0530

    Update readme

--- frontend/farm-website/src/components/ContentContainer.js
+++ frontend/farm-website/src/components/ContentContainer.js
@@ -2,7 +2,7 @@
 
 const ContentContainer = ({ children }) => {
   return (
-    <div className="absolute top-1/2 right-[5%] transform -translate-y-1/2 w-[55%] h-[80%] bg-white/80 rounded-2xl backdrop-blur-sm p-8 overflow-hidden shadow-content flex flex-col">
+    <div className="absolute top-1/2 right-[5%] transform -translate-y-1/2 w-[55%] h-[80%] bg-white/80 rounded-2xl backdrop-blur-sm p-8 overflow-scroll shadow-content flex flex-col">
       {children}
     </div>
   );
